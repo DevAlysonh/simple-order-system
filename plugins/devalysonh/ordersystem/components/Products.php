6c86b43ea8341a29e7fdfde6b49e61c450c7c636
Dev.Alysonh <dev.alysonh@gmail.com>
2024-07-06 13:15:08 -0300

    Enable delete product

--- plugins/devalysonh/ordersystem/components/Products.php
+++ plugins/devalysonh/ordersystem/components/Products.php
@@ -41,6 +41,21 @@ class Products extends ComponentBase
 		return redirect()->back();
 	}
 
+	public function onDeleteProduct()
+	{
+		$productId = post('id');
+        $product = Product::find($productId);
+
+        if (!$product) {
+			Flash::error('Produto inválido');
+			return redirect()->back();
+		}
+
+		$product->delete();
+		Flash::success('O produto foi deletado');
+		return redirect()->back();
+	}
+
     /**
      * @link https://docs.octobercms.com/3.x/element/inspector-types.html
      */
